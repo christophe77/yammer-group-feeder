cb1a0eee11588062cffb0046fa9876ec199b9c32
christopheb <chbellec@airfrance.fr>
2017-01-04 10:58:04 +0100

    Remove flat-db and add fs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Config
-var yammerGroupId = "10215902";
-var yammerOauth2Token = "4006-Ki5YBLdI8owX2XFHppo0A";
+var yammerGroupId = "";
+var yammerOauth2Token = "";
 var RssCheckIntervalInMn = 1;
 var RssFeedsList = [
 	'http://www.alsacreations.com/rss/apprendre.xml',
@@ -10,8 +10,6 @@ var RssFeedsList = [
 //Fix the maxlisteners bug
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 
-// flat-file-db
-var flatfile = require('flat-file-db');
 // we create a daily database
 function dayDB(){
 	var path = require('path');
@@ -27,10 +25,8 @@ function dayDB(){
 
 	if(mm<10) {
 		mm='0'+mm
-	} 
-	
+	} 	
 	today = appDir+'\\db\\'+mm+'-'+dd+'-'+yyyy+'.db';
-	console.log(today);
 	return(today);
 }
 
@@ -51,8 +47,6 @@ var rule = new schedule.RecurrenceRule();
 rule.minute = new schedule.Range(0, 59, RssCheckIntervalInMn);
  
 var j = schedule.scheduleJob(rule, function(){	
-	// Daily database
-	var db = flatfile.sync(dayDB());
 	
 	// Loop through RSS Feed links
     for (i = 0; i < RssFeedsList.length; i++) {
@@ -67,11 +61,8 @@ var j = schedule.scheduleJob(rule, function(){
 			console.log(error);
 		});
 		req.on('response', function (res) { 
-
-			var stream = this;
-			
+			var stream = this;	
 			if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
-
 			stream.pipe(feedparser);
 		});
 
@@ -86,33 +77,41 @@ var j = schedule.scheduleJob(rule, function(){
 
 		  while (item = stream.read()) {
 			// check if the link has already been posted
-			db.on('open', function() {
-				if(db.has(item.link) === false) {
-					// Add link to database
-					db.put('itemLink', item.link);  // store some data
-					// send message to yammer  
-					var message = item.title + ' | ' + item.link;
-					
-					// send message to yammer
-					client.messages.create({
-						group_id: yammerGroupId, 
-						body: message
-					}, 
-					function(error, data) {
-						if(error) {
-							console.log("There was an error posting the data");
-							console.log(error);
-						}
-											
-						else {
-								console.log("Data posted");
-								//console.log(data);
+			
+			if(item.link){
+				var cLink = item.link;
+				var cTitle = item.title;
+				var fs = require('fs');				
+				fs.readFile(dayDB(), function (err, data) {
+					if (err) throw err;
+					if(data.indexOf(cLink) < 0){
+						fs.appendFile(dayDB(), cLink, function (err) {
+							if (err) throw err;
+						});
+						// Send to yammer
+						var message = cTitle + ' | ' + cLink;
+						client.messages.create({
+							group_id: yammerGroupId, 
+							body: message
+						}, 
+						function(error, data) {
+							if(error) {
+								console.log("There was an error posting the data");
+								console.log(error);
 							}
-					})
-				}
-			});
-			db.close();
-		  }
+												
+							else {
+									console.log("Data posted");
+									//console.log(data);
+							}
+						})
+					}
+
+				});
+
+				
+			}
+		  }		  
 		});
 	}
 });
